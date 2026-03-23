d1fa8bfa507cde874974344d78ecd5afadf2ea98
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-03-23 16:44:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let checkOrder = [];
+
+const toggles = document.querySelectorAll(".toggle");
+
+toggles.forEach((toggle) => {
+	toggle.addEventListener("change", () => {
+		if(toggle.checked) {
+			if(checkOrder.length < 2) {
+				checkOrder.push(toggle.id);
+			} else {
+				const firstChecked = checkOrder[0];
+				const firstCheckBox = document.getElementById(firstChecked);
+
+				firstCheckBox.checked = false;
+				checkOrder.shift();
+
+				checkOrder.push(toggle.id);
+			}
+		} else {
+			checkOrder = checkOrder.filter(id => id !== toggle.id);
+		}
+	})
+})
