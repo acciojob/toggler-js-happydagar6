d2a99d608ca456e140c52e08ba85b3b6d3813e80
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-03-23 17:37:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,20 +7,14 @@ const toggles = document.querySelectorAll(".toggle");
 toggles.forEach(toggle => {
     toggle.addEventListener("change", (e) => {
         
-        // Check if all three checkboxes are currently active
         if (good.checked && cheap.checked && fast.checked) {
             
-            // If the user just clicked 'good', uncheck 'fast'
             if (e.target === good) {
                 fast.checked = false;
             }
-            
-            // If the user just clicked 'cheap', uncheck 'good'
             if (e.target === cheap) {
                 good.checked = false;
             }
-
-            // If the user just clicked 'fast', uncheck 'cheap'
             if (e.target === fast) {
                 cheap.checked = false;
             }
